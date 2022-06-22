472d7ab7b4b80ef9d73857713f114c8a9bdeb0b7
odevpedro <pedrosschmidt2@gmail.com>
2022-06-21 21:36:50 -0300

    trabalhando com params

--- fundamentos-nodejs/index.js
+++ fundamentos-nodejs/index.js
@@ -1,6 +1,8 @@
-const { response } = require('express');
+const { response, request, query } = require('express');
 const express = require('express')
 const app = express();
+
+app.use(express.json())
 
 
 
@@ -13,21 +15,28 @@ const app = express();
  */
 
 app.get("/courses", (req, res) => {
-    return res.json([
-        "curso 1",
-        "curso 2", 
-        "curso 3"
+    const query = req.query;
+    console.log(query);
+    return res.json([ "curso 1", "curso 2", "curso 3"
     ]);
 })
 
 
+//Route params, identificar um recurso editar/deletar/buscar
+//Query params, Paginação / Filtro
+//Body Params, objetos passados para inserção/algeração
+
 
 app.post("/courses", (req, res) =>{
+    const body = req.body;
+    console.log(body )
     return res.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
 })
 
 
 app.put("/courses/:id", (req, res) =>{
+    const {id} = req.params;
+    console.log(id);
     return res.json(["OutraCoisa", "Curso2", "Curso3", "Curso4"]);
 
 })
@@ -43,6 +52,7 @@ app.patch("/courses/:id", (req, res) => {
 
 })
 app.listen(3333);
+
 
 
 
